7160bd58bc041eb0ac4f6b665acfe00eb4881ffe
wellington pinheiro <wellington.pinheiro@live.com>
2025-03-09 10:06:50 -0300

    Implementando função adicionarAmigo: adiciona nome, valida entrada e evita duplicatas

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,19 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+function adicionarAmigo() {
+    const input = document.getElementById('amigo');
+    const nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Digite um nome válido!");
+        return;
+    }
+
+    if (amigos.includes(nome)) {
+        alert("Este nome já foi adicionado!");
+        return;
+    }
+
+    amigos.push(nome);
+    input.value = "";
+    atualizarLista();
+}
